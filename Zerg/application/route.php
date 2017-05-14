fa4e2b95d0594d0b74ef95b020333791b9d9fad6
demoonwu <demoonwu@foxmail.com>
2017-05-14 23:48:49 +0800

    修改router文件
    使用三种request方式（依赖注入、input助手函数、instance实例化Request）获取参数，直接获取get参数

--- Zerg/application/route.php
+++ Zerg/application/route.php
@@ -9,13 +9,7 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
+use think\Route;
 
-];
+Route::rule('hello','sample/Test/hello');
+Route::post('getvar/:id','sample/Test/getvar');
--- Zerg/application/sample/controller/Test.php
+++ Zerg/application/sample/controller/Test.php
@@ -1,5 +1,6 @@
 <?php 
 	namespace app\sample\controller;
+	use think\Request;
 
 	/**
 	* 作者：吴咏蔚
@@ -9,6 +10,19 @@
 		public function hello()
 		{
 			return "hello 吴咏蔚";
+		}
+		public function getvar(Request $request){
+			//var_dump($_GET);
+			//return "测试通过";
+			// 使用request方法
+			// $all=Request::instance()->param();
+			// var_dump($all);
+			//3、依赖注入方法
+			// $all=$request->param();
+			// 使用助手函数
+			$all=input('param.');
+			var_dump($all);
+
 		}
 	}
 
